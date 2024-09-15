78e55988e3fee308970d12c4ea40b8f170b55daf
mikedarling12 <mdarl1ng@byui.edu>
2024-09-14 19:04:07 -0700

    v0.2
    Most basic functionality is present.
    Needs functionality to handle both a colored and generic die sum in next version.

--- Quixx_Server/scripts/cellToggle.js
+++ Quixx_Server/scripts/cellToggle.js
@@ -1,25 +1,50 @@
+let coloredDicePickAvailable = false;
+let genericDicePickAvailable = false;
+
 function toggleCell(id) {
     var cell = document.getElementById(id);
-    if (cell.classList.contains('notSelected')) {
+    if (cell.classList.contains('notSelected') && cell.classList.contains("clickable") && coloredDicePickAvailable) {
         cell.classList.remove('notSelected');
         cell.classList.add('selected');
         cell.querySelector('.mark-button').classList.add('marked');
         let color = cell.querySelector('.mark-button').classList[0];
+        // if (cell.textContent == 12 && (color == "red" || color == "yellow")) {
+        //     console.log("Selecting " + color + 13);
+        //     document.getElementById(color + 13).classList.add('marked');
+        // }
+        // if (cell.textContent == 2 && (color == "blue" || color == "green")) {
+        //     console.log("Selecting " + color + 13);
+        //     document.getElementById(color + 13).classList.add('marked');
+        // }
         updateTotal(color);
+        coloredDicePickAvailable = false;
+        // removeAvailabilityFromCells();
     }
-    else if (cell.classList.contains('selected')) {
+    else if (cell.classList.contains('selected') && cell.classList.contains("clickable")) {
         cell.classList.remove('selected');
         cell.classList.add('notSelected');
         cell.querySelector('.mark-button').classList.remove('marked');
         let color = cell.querySelector('.mark-button').classList[0];
         updateTotal(color);
+        coloredDicePickAvailable = true;
     }
+}
+
+function countSelectedCellsInRow(color) {
+    var total = 0;
+    for (var i = 2; i <= 13; i++) {
+        var cell = document.getElementById(color + i);
+        if (!cell.classList.contains('notSelected')) {
+            total += 1;
+        }
+    }
+    return total;
 }
 
 function updateTotal(color) {
     var total = 0;
     let numSelected = 0;
-    for (var i = 2; i <= 13; i++) {
+    for (var i = 2; i < 14; i++) {
         var cell = document.getElementById(color + i);
         if (!cell.classList.contains('notSelected')) {
             total += ++numSelected;
@@ -29,11 +54,75 @@ function updateTotal(color) {
     updateOverallTotal();
 }
 
+function calculatePenalties() {
+    let totalPenalty = 0;
+    for (let penalty_i = 1; penalty_i < 5; penalty_i++) {
+        if (document.getElementById("penalty" + penalty_i).checked) {
+            totalPenalty += 5;
+        }
+    }
+    return totalPenalty;
+}
+
 function updateOverallTotal() {
     let redTotal = parseInt(document.getElementById('redTotal').textContent);
     let yellowTotal = parseInt(document.getElementById('yellowTotal').textContent);
     let greenTotal = parseInt(document.getElementById('greenTotal').textContent);
     let blueTotal = parseInt(document.getElementById('blueTotal').textContent);
-    let overallTotal = redTotal + yellowTotal + greenTotal + blueTotal;
+    let penaltyTotal = calculatePenalties();
+    let overallTotal = redTotal + yellowTotal + greenTotal + blueTotal - penaltyTotal;
     document.getElementById('overallTotal').textContent = overallTotal;
+}
+
+function markCell(id, color) {
+    if (cellStillPossible(id, color)) {
+        document.getElementById(id).classList.add("clickable");
+    }
+}
+
+function cellStillPossible(id, color) {
+    if (document.getElementById(id).classList.contains("clickable") || document.getElementById(id).classList.contains("selected")) {
+        return false;
+    }
+    var prospectiveCellValue = parseInt(document.getElementById(id).textContent);
+    console.log(color);
+    console.log(prospectiveCellValue);
+
+    // Based on which color the prospective cell is, check for any cell that would block the cell from being selected.
+    if (color == "red" || color == "yellow") {
+        for (let valueToCheck = prospectiveCellValue + 1; valueToCheck < 12; valueToCheck++) {
+            console.log(document.getElementById(color + valueToCheck));
+            if (document.getElementById(color + valueToCheck).classList.contains("selected")) {
+                return false;
+            }
+        }
+        if (prospectiveCellValue == 12 && countSelectedCellsInRow(color) < 5) {
+            return false;
+        }
+    } else if (color == "blue" || color == "green") {
+        for (let valueToCheck = prospectiveCellValue - 1; valueToCheck > 3; valueToCheck--) {
+            console.log(document.getElementById(color + valueToCheck));
+            if (document.getElementById(color + valueToCheck).classList.contains("selected")) {
+                return false;
+            }
+        }
+        if (prospectiveCellValue == 2 && countSelectedCellsInRow(color) < 5) {
+            return false;
+        }
+    } else { console.log("There was an exception here.")}
+    return true;
+}
+
+function removeAvailabilityFromCells() {
+    var markedElements = document.querySelectorAll(".clickable");
+    var totalElements = markedElements.length;
+    for (let i = 0; i < totalElements; i++) {
+        markedElements[i].classList.remove('clickable');   
+    }
+}
+
+function lockIn() {
+    removeAvailabilityFromCells();
+    coloredDicePickAvailable = false;
+    genericDicePickAvailable = false;
 }
---
+++ Quixx_Server/scripts/dice.js
@@ -0,0 +1,61 @@
+// This function will cause all available dice to reroll themselves, then update their visual values.
+function rollDice() {
+    removeAvailabilityFromCells();
+    var whiteDice1 = document.getElementById("whiteDice1");
+    var whiteDice2 = document.getElementById("whiteDice2");
+    var redDice = document.getElementById("redDice");
+    var yellowDice = document.getElementById("yellowDice");
+    var greenDice = document.getElementById("greenDice");
+    var blueDice = document.getElementById("blueDice");
+    whiteDice1.textContent = randomizeDice();
+    whiteDice2.textContent = randomizeDice();
+    redDice.textContent = randomizeDice();
+    yellowDice.textContent = randomizeDice();
+    greenDice.textContent = randomizeDice();
+    blueDice.textContent = randomizeDice();
+
+    markAllAvailableCells([parseInt(whiteDice1.textContent), parseInt(whiteDice2.textContent), 
+                        parseInt(redDice.textContent), parseInt(yellowDice.textContent), 
+                        parseInt(greenDice.textContent), parseInt(blueDice.textContent)]);
+    coloredDicePickAvailable = true;
+}
+
+function markAllAvailableCells(diceResults) {
+    for (let generic_dice_i = 0; generic_dice_i < 2; generic_dice_i++) {
+        markCell(getAvailableCellID("red", diceResults[generic_dice_i], diceResults[2]), "red");
+        markCell(getAvailableCellID("yellow", diceResults[generic_dice_i], diceResults[3]), "yellow");
+        markCell(getAvailableCellID("green", diceResults[generic_dice_i], diceResults[4]), "green");
+        markCell(getAvailableCellID("blue", diceResults[generic_dice_i], diceResults[5]), "blue");
+    }
+}
+
+function getAvailableCellID(color, die1, die2) {
+    var value = die1 + die2;
+    var id = color + value;
+    return id;
+}
+
+// This function will roll a single die to determine its new value.
+function randomizeDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+// This function will cause all available dice to reroll themselves, then update their visual values.
+function setDiceEnd() {
+    var whiteDice1 = document.getElementById("whiteDice1");
+    var whiteDice2 = document.getElementById("whiteDice2");
+    var redDice = document.getElementById("redDice");
+    var yellowDice = document.getElementById("yellowDice");
+    var greenDice = document.getElementById("greenDice");
+    var blueDice = document.getElementById("blueDice");
+    whiteDice1.textContent = 6;
+    whiteDice2.textContent = 1;
+    redDice.textContent = 6;
+    yellowDice.textContent = 6;
+    greenDice.textContent = 1;
+    blueDice.textContent = 1;
+    
+    markAllAvailableCells([parseInt(whiteDice1.textContent), parseInt(whiteDice2.textContent), 
+        parseInt(redDice.textContent), parseInt(yellowDice.textContent), 
+        parseInt(greenDice.textContent), parseInt(blueDice.textContent)]);
+}
